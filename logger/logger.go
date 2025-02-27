5f0821ce85f33603311bedeceb3b84c2b33a1c42
Raza Balbale <ribalbale@gmail.com>
2025-02-26 19:15:03 -0600

    Updated logger formatting and writing to file.

--- logger/logger.go
+++ logger/logger.go
@@ -1,24 +1,29 @@
 package logger
 
 import (
+	"context"
 	"os"
 	"time"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 var Logger zerolog.Logger
 
+type contextKey string
+
+const RequestIDKey contextKey = "requestID"
+
 func InitLogger() {
-	// Set up pretty console logging for development
 	consoleWriter := zerolog.ConsoleWriter{
 		Out:        os.Stdout,
 		TimeFormat: time.RFC3339,
 	}
 
-	// Create multi-writer for both console and file
-	_, err := os.OpenFile(
+	file, err := os.OpenFile(
 		"app.log",
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
 		0664,
@@ -27,38 +32,65 @@ func InitLogger() {
 		log.Fatal().Err(err).Msg("Could not open log file")
 	}
 
-	// Set global logger
-	Logger = zerolog.New(consoleWriter).
+	multi := zerolog.MultiLevelWriter(consoleWriter, file)
+
+	Logger = zerolog.New(multi).
 		With().
 		Timestamp().
 		Caller().
 		Logger()
 
-	// Set log level based on environment
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	if os.Getenv("ENV") == "development" {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	}
 
-	// Set default logger
 	log.Logger = Logger
 }
 
-// LogRequest logs incoming HTTP requests
-func LogRequest(method, path string, duration time.Duration, status int) {
-	Logger.Info().
+// LogRequest now includes error information and request ID
+func LogRequest(method, path string, duration time.Duration, status int, ctx context.Context, err error) {
+	requestID := ctx.Value(RequestIDKey).(string)
+
+	event := Logger.Info()
+	if status >= 500 {
+		event = Logger.Error()
+	}
+
+	event.
+		Str("requestId", requestID).
 		Str("method", method).
 		Str("path", path).
 		Dur("duration", duration).
-		Int("status", status).
-		Msg("Request processed")
+		Int("status", status)
+
+	if err != nil {
+		event.Err(err)
+	}
+
+	event.Msg("Request processed")
 }
 
-// LogError logs error messages with context
-func LogError(err error, message string, fields map[string]interface{}) {
-	event := Logger.Error().Err(err)
-	for key, value := range fields {
-		event = event.Interface(key, value)
+// LogError now includes request ID from context
+func LogError(ctx context.Context, err error, msg string, fields map[string]interface{}) {
+	requestID := ctx.Value(RequestIDKey).(string)
+
+	event := log.Error().
+		Str("requestId", requestID).
+		Err(err)
+
+	if fields != nil {
+		event.Fields(fields)
 	}
-	event.Msg(message)
+
+	event.Msg(msg)
+}
+
+// Middleware to add request ID to context
+func RequestIDMiddleware(c *fiber.Ctx) error {
+	requestID := uuid.New().String()
+	ctx := context.WithValue(c.Context(), RequestIDKey, requestID)
+	c.SetUserContext(ctx)
+	c.Set("X-Request-ID", requestID)
+	return c.Next()
 }
